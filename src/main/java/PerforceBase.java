35b6b7949525d3b338ef495629dbb8ba1128ead9
bong9 <mmxbong9@gmail.com>
2025-05-10 15:33:31 +0900

    .txt 파일에서 퍼포스 접속 관련 설정 값을 로드.

--- src/main/java/PerforceBase.java
+++ src/main/java/PerforceBase.java
@@ -3,7 +3,12 @@ import com.perforce.p4java.option.UsageOptions;
 import com.perforce.p4java.server.IOptionsServer;
 import com.perforce.p4java.server.ServerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
@@ -19,44 +24,76 @@ public abstract class PerforceBase {
 
     /**
      * The server URI to be used for the demo run. This is retrieved
-     * from the current value of the system property com.perforce.p4javademo.serverUri;
+     * from the current value of the system property com.perforce.p4settings.serverUri;
      * if no such system property has been set, the default used is
      * p4java://localhost:1666.
      */
-    protected static String serverUri = System.getProperty(
-            "com.perforce.p4javademo.serverUri", "p4java://your_server_address:1666");
-
+    protected static final String serverUri;
     /**
      * The user name to be used for the demo run. This is retrieved
-     * from the current value of the system property com.perforce.p4javademo.userName;
+     * from the current value of the system property com.perforce.p4settings.userName;
      * if no such system property has been set, the default used is
      * "P4javaDemoUser".
      */
-    protected static String userName = System.getProperty(
-            "com.perforce.p4javademo.userName", "your_id");
-
+    protected static final String userName;
     /**
      * The Perforce client name to be used for the demo run. This is retrieved
-     * from the current value of the system property com.perforce.p4javademo.clientName;
+     * from the current value of the system property com.perforce.p4settings.clientName;
      * if no such system property has been set, the default used is
-     * "p4javademo".
+     * "p4settings".
      */
-    protected static String clientName = System.getProperty(
-            "com.perforce.p4javademo.clientName", "your_workspace_name");
-
+    protected static final String clientName;
     /**
      * The Perforce user password to be used for the demo run. This is retrieved
-     * from the current value of the system property com.perforce.p4javademo.password;
+     * from the current value of the system property com.perforce.p4settings.password;
      * if no such system property has been set, the default used is
      * "none" (which is not the same as not having a password).
      */
-    protected static String password = System.getProperty(
-            "com.perforce.p4javademo.password", "your_password");
+    protected static final String password;
+
+    private static final String PROP_PREFIX = "com.perforce.p4settings.";
+    private static final String CONFIG_FILE = "PerforceSettings.txt";
+
+    private static final Properties fileProps = new Properties();
+
+    static {
+        loadFileProperties();
+
+        serverUri  = loadConfig("serverUri", "p4java://your_ip_address:1666");
+        userName   = loadConfig("userName", "your_id");
+        clientName = loadConfig("clientName", "your_workspace_name");
+        password   = loadConfig("password", "your_password");
+    }
+
+    private static void loadFileProperties() {
+        Path cfg = Paths.get(CONFIG_FILE);
+        if (Files.exists(cfg)) {
+            try (InputStream in = Files.newInputStream(cfg)) {
+                fileProps.load(in);
+            } catch (IOException e) {
+                System.err.println("설정 파일 로드 실패: " + e.getMessage());
+            }
+        }
+    }
+
+    private static String loadConfig(String key, String defaultValue) {
+        String sys = System.getProperty(PROP_PREFIX + key);
+
+        if (sys != null && !sys.isEmpty()) {
+            return sys;
+        }
+
+        String fileVal = fileProps.getProperty(key);
+        if (fileVal != null && !fileVal.isEmpty()) {
+            return fileVal;
+        }
+        return defaultValue;
+    }
 
     /**
      * Get an IOptionsServer object from the P4Java server factory and connect to it.
      * The URI passed to the server factory is cobbled together from the system
-     * property com.perforce.p4javademo.serverUri; if the property is not set,
+     * property com.perforce.p4settings.serverUri; if the property is not set,
      * the URI defaults to p4java://localhost:1666.
      *
      * @param props if not null, P4Java properties object to pass to the P4Java
